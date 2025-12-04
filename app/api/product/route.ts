033879180cce28ee2c2438a20a30080a1c47b188
Ahmad-Rofiki <ahmadrofiki6146@gmail.com>
2025-12-04 17:50:16 +0700

    end

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -3,13 +3,21 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
   {
     id: 1,
-    name: "Product 1",
-    price: 100,
+    title: "Product 1",
+    price: 654,
+    img: "https://www.nike.com/id/t/air-max-tl-2-5-shoes-mFumxy6c/IM6647-030",
   },
   {
     id: 2,
-    name: "Product 2",
-    price: 200,
+    title: "Product 2",
+    price: 711,
+    img: "https://www.nike.com/id/t/air-max-plus-iii-shoes-Qw64gh/CJ9684-002",
+  },
+  {
+    id: 3,
+    title: "Product 3",
+    price: 991,
+    img: "https://www.nike.com/id/t/sb-air-jordan-4-shoes-Pmb442/DR5415-100",
   },
 ];
 
@@ -21,11 +29,15 @@ export async function GET(request: NextRequest) {
     const detailProduct = data.find((item) => item.id == Number(id));
     if (detailProduct) {
       return NextResponse.json({
-        status: 200, message: "Success", detailProduct});
+        status: 200,
+        message: "Success",
+        detailProduct,
+      });
     }
   }
   return NextResponse.json({
-    status: 404, message: "id not found", data: {
-
-  } });
+    status: 404,
+    message: "id not found",
+    data: {},
+  });
 }
